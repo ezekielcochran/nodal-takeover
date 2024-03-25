66641c0583b7b055126734fe42319ccb9d1e8610
Ezekiel Cochran <zeke.j.cochran@gmail.com>
2024-03-25 18:14:10 -0500

    Add Pause Menu to Level 2
    using UnityEditor.Experimental.GraphView was throwing a compiler error: I commented it out because it seemed unused
    
    Some quirks to work out:
    	game can be played behind the pause menu
    	scripting on pause menu buttons is questionable... uses scripts meant for level selector
    	pause button/menu doesn't simply scale on larger screens, but appears smaller

--- Assets/LevelsMenu.cs
+++ Assets/LevelsMenu.cs
@@ -14,12 +14,18 @@ public class LevelsMenu : MonoBehaviour
     public void ToMainMenu()
     {
         SceneManager.LoadSceneAsync("Main Menu");
+        Debug.Log("Main Menu");
     }
 
     public void ToLevel(int levelNumber) {
 		// SceneManager.LoadSceneAsync(sceneID);
 		SceneManager.LoadSceneAsync("Level " + levelNumber);
 	}
+
+    // Seems like a tacky way to do this... please fix later, this is just for prototype
+    public void ToLevelsMenu() {
+        SceneManager.LoadSceneAsync("Level Select");
+    }
 
     // Update is called once per frame
     // void Update()
